d55aeb3cfd2016e4a32274eb9d24a23f9298399f
Randall Taylor <randyisbusy@gmail.com>
2019-06-19 20:25:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
   puts "Hi, my name is #{name}.\n"
 end
+def introduction_with_language(name, Language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
